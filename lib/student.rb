6a2ff8fa9ea8fa2516ae6ff1edd1891ccf1feadc
Serena Berry <siberry226@gmail.com>
2019-03-18 19:52:45 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+  def initialize
+    super
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
